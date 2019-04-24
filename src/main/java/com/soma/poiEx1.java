6b7b158d273793728fbb7b1cb10c1ca7524267f6
Soma <Soma@Soma-001>
2019-04-24 23:05:19 +0800

    xlsx Ex1

---
+++ src/main/java/com/soma/poiEx1.java
@@ -0,0 +1,41 @@
+package com.soma;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class poiEx1 {
+
+	public static void main(String[] args) throws Exception {
+		File myFile = new File("F:/xlsx/sample.xlsx");
+		FileInputStream fis = new FileInputStream(myFile);
+
+		XSSFWorkbook myWorkBook = new XSSFWorkbook(fis); // Finds the workbook instance for XLSX file
+
+		XSSFSheet mySheet = myWorkBook.getSheetAt(0); // Return first sheet from the XLSX workbook
+
+		GetColor RGB = new GetColor();
+		
+		for (int i = 0; i < 270; i++) {
+			XSSFRow row = mySheet.createRow(i);
+			row.setHeight((short) 50);
+		}
+
+		for (int i = 0; i < 480; i++) {
+			mySheet.setColumnWidth(i, 100);
+		}
+
+		FileOutputStream fos = new FileOutputStream(myFile);
+		myWorkBook.write(fos);
+
+		myWorkBook.close();
+		fos.close();
+		fis.close();
+
+	}
+
+}
