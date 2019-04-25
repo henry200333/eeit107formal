323cd0655e199a631536ea7f87479eed5a6e5393
henry <henry@MSI>
2019-04-25 19:01:22 +0800

    add index

---
+++ src/main/java/org/iii/seaotter/SeaOtterApplication.java
@@ -0,0 +1,15 @@
+package org.iii.seaotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //­­¨î¡B¼Ð°O
+public class SeaOtterApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SeaOtterApplication.class, args);
+		
+	}
+
+}
