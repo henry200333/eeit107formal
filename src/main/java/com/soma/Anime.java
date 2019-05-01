f74e5756207996e65e5f9415860635c25294baf4
Soma <Soma@Soma-001>
2019-05-01 13:13:02 +0800

    upload soma.Anime

---
+++ src/main/java/com/soma/Anime.java
@@ -0,0 +1,41 @@
+package com.soma;
+
+public class Anime {
+	
+	private Integer id;
+	private String name;
+	private java.util.Date releaseDate;
+	private String genre;
+	private int episode;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public java.util.Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(java.util.Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public int getEpisode() {
+		return episode;
+	}
+	public void setEpisode(int episode) {
+		this.episode = episode;
+	}
+}
