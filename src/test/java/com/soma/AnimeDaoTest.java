190b3c07bf09403c28a663ba30f1fe7443c7af38
Soma <Soma@DESKTOP-DQE796U>
2019-05-02 20:09:22 +0800

    update AnimeDaoTest.java

---
+++ src/test/java/com/soma/AnimeDaoTest.java
@@ -0,0 +1,27 @@
+package com.soma;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.iii.seaotter.album.Album;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AnimeDaoTest {
+	@Autowired
+	private AnimeDao animeDao;
+	
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		List<Anime> animeDaoList = animeDao.findAll();
+		System.out.println(animeDaoList);
+	}
+
+}
