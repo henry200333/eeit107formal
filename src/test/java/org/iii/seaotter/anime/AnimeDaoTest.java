39aee546c72d36aebd6f73715c8b6473980a6b05
User <User@EEIT-PC>
2019-05-08 05:19:51 +0800

    update AnimeDaoTest V1.2.0

--- src/test/java/org/iii/seaotter/anime/AnimeDaoTest.java
+++ src/test/java/org/iii/seaotter/anime/AnimeDaoTest.java
@@ -1,5 +1,9 @@
 package org.iii.seaotter.anime;
 
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.iii.seaotter.anime.Anime;
@@ -20,7 +24,44 @@ public class AnimeDaoTest {
 	public void testProcedure() {
 //		fail("Not yet implemented");
 		List<Anime> animeDaoList = animeDao.findAll();
-		System.out.println(animeDaoList);
+		assertEquals(5, animeDaoList.size());
+		//test findAll() End
+		
+		Anime anime = animeDao.findOne(2);
+		assertEquals("魔法少女小圓", anime.getName());
+		//test findOne() End
+		
+		anime.setId(100);
+		anime.setName("Another");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date1;
+		try {
+			date1 = sdf.parse("2000-01-09");
+			anime.setReleaseDate(new java.sql.Date(date1.getTime()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		anime.setGenre("驚悚");
+		anime.setEpisode(12);
+		animeDao.insert(anime);
+		animeDaoList = animeDao.findAll();
+		assertEquals(6, animeDaoList.size());
+		//test insert() End
+		
+		try {
+			date1 = sdf.parse("2012-01-09");
+			anime.setReleaseDate(new java.sql.Date(date1.getTime()));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		animeDao.update(anime);
+		anime = animeDao.findOne(100);
+		assertEquals("驚悚", anime.getGenre());
+		//test update() findOne() End
+		
+		animeDao.delete(100);
+		animeDaoList = animeDao.findAll();
+		assertEquals(5, animeDaoList.size());
 	}
 
 }
