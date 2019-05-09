e804841f7cc00627bfb2948dc46bbbc68e822814
lcy11 <lcy11@Kallil>
2019-05-09 20:32:42 +0800

    Kallil 0509.

--- src/test/java/com/kallil/CountryDAOTest.java
+++ src/test/java/com/kallil/CountryDAOTest.java
@@ -1,14 +1,30 @@
 package com.kallil;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.kallil.CountryDAO;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class CountryDAOTest {
+	@Autowired
+	private CountryDAO countryDao;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindAll() {
+//		fail("Not yet implemented");
+		List<CountryBean> countryDaoList = countryDao.findAll();
+		assertEquals(5,countryDaoList.size());
+		
 	}
 
 }
