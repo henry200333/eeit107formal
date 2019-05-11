9656bd111ac4983f3b2682c7e5e2cb913328cd1d
Soma <Soma@Soma-001>
2019-05-11 23:53:57 +0800

    update import.sql with TABLE performance and artists

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,7 +17,7 @@ INSERT INTO anime values (5,'動物朋友','2017-01-10', '日常', 13);
 
 DROP TABLE artists;
 CREATE TABLE artists (
-id integer IDENTITY(1,1) primary key,
+a_id integer IDENTITY(1,1) primary key,
 name varchar(30),
 join_time datetime,
 manager integer,
@@ -27,3 +27,16 @@ INSERT INTO artists values ('管理員1', '2019-5-10 23:50:00', 1);
 INSERT INTO artists values ('周杰倫', '2019-5-11 13:12:05', 0);
 INSERT INTO artists values ('蔡依林', '2019-5-12 09:10:34', 0);
 INSERT INTO artists values ('王心凌', '2019-5-13 21:55:22', 0);
+
+DROP TABLE performance;
+CREATE TABLE performance (
+p_id integer IDENTITY(1,1) primary key,
+a_name varchar(30),
+p_name varchar(30),
+p_url varchar(200),
+update_time datetime,
+);
+
+INSERT INTO performance values ('周杰倫', '千里之外', 'https://youtu.be/ocDo3ySyHSI', '2019-5-11 13:15:20');
+INSERT INTO performance values ('周杰倫', '聽媽媽的話', 'https://youtu.be/_B8RaLCNUZw', '2019-5-11 13:16:34');
+INSERT INTO performance values ('周杰倫', '牛仔很忙', 'https://youtu.be/9o20U91aJdg', '2019-5-11 13:18:02');
