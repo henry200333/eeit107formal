81ce98420f6a9eba9bdea43d71409b6bd540d918
Soma <Soma@Soma-001>
2019-05-12 00:17:36 +0800

    update Artists.java & ArtistsDao.java ArtistsDao still need to be edited.

---
+++ src/main/java/org/iii/seaotter/artists/ArtistsDao.java
@@ -0,0 +1,25 @@
+package org.iii.seaotter.artists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArtistsDao {
+
+	public void insert(Artists artists) {
+	}
+	
+	public void update(Artists artists) {
+	}
+	
+	public void delete(Integer id) {
+	}
+	
+	public Artists findOne(Integer id) {
+		return new Artists();
+	}
+	
+	public List<Artists> findAll(){
+		List<Artists> artistsList = new ArrayList<Artists>();
+		return artistsList;
+	}
+}
