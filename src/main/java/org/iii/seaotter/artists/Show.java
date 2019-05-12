0d77f929eba20d03fc3c491090164f8e48d0a3af
ken <ken@192.168.100.3>
2019-05-12 21:15:22 +0800

    Show

---
+++ src/main/java/org/iii/seaotter/artists/Show.java
@@ -0,0 +1,50 @@
+package org.iii.seaotter.artists;
+
+import java.sql.Timestamp;
+
+public class Show {
+	
+	private Integer id;
+	private String showName;
+	private String artist;
+	private String type;
+	private Timestamp date;
+	
+	@Override
+	public String toString() {
+		return "Show [id=" + id + ", showName=" + showName + ", artist=" + artist + ", type=" + type + ", date=" + date
+				+ "]";
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getShowName() {
+		return showName;
+	}
+	public void setShowName(String showName) {
+		this.showName = showName;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Timestamp getDate() {
+		return date;
+	}
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+
+	
+}
