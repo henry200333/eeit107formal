c4b6a7a6ed4de3eb6e19578d9d0f0c57e3d2a831
ken <ken@192.168.100.3>
2019-05-12 21:41:11 +0800

    Show Dao

--- src/main/java/org/iii/seaotter/artists/ArtistsDao.java
+++ src/main/java/org/iii/seaotter/artists/ArtistsDao.java
@@ -3,7 +3,16 @@ package org.iii.seaotter.artists;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ArtistsDao {
+	
+	@Autowired 
+	private DataSource dataSource;
 
 	public void insert(Artists artists) {
 	}
