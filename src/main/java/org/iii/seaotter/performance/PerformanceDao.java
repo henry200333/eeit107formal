8efe9f022eb27758fba891e34fdde99e17fa8715
Soma <Soma@Soma-001>
2019-05-14 22:55:19 +0800

    add @repository into PerformanceDao

--- src/main/java/org/iii/seaotter/performance/PerformanceDao.java
+++ src/main/java/org/iii/seaotter/performance/PerformanceDao.java
@@ -11,7 +11,9 @@ import javax.sql.DataSource;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class PerformanceDao {
 	
 	@Autowired 
