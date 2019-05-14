e7fdfd39246decafd9d7db686aac7b649a1e3dde
User <User@EEIT-PC>
2019-05-14 20:31:06 +0800

    update ArtistsDao.java

--- src/main/java/org/iii/seaotter/artists/ArtistsDao.java
+++ src/main/java/org/iii/seaotter/artists/ArtistsDao.java
@@ -1,5 +1,8 @@
 package org.iii.seaotter.artists;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +16,32 @@ public class ArtistsDao {
 	
 	@Autowired 
 	private DataSource dataSource;
+	
+	private static final String FIND_BY_ID = "SELECT * FROM artists WHERE a_id=?";
+	private static final String FIND_BY_NAME = "SELECT * FROM artists WHERE name=?";
+	private static final String FIND_ALL = "SELECT * FROM artists";
+	private static final String UPDATE =
+			"UPDATE artists SET name=?, join_time=?, manager=? WHERE a_id=?";
+	private static final String INSERT =
+			"INSERT INTO artists (name, join_time, manager) VALUES (?, ?, ?)";
+	private static final String DELETE = "DELETE FROM artists WHERE a_id=?";
 
 	public void insert(Artists artists) {
+		
 	}
 	
 	public void update(Artists artists) {
+		
 	}
 	
 	public void delete(Integer id) {
+		try (Connection connection = dataSource.getConnection();) {
+			PreparedStatement pstmt = connection.prepareStatement(DELETE);
+			pstmt.setInt(1, id);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Artists findOne(Integer id) {
