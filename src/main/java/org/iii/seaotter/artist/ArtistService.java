4b868b2fab7cd834a82336108d33db1ec803a2bf
User <User@EEIT-PC>
2019-05-15 15:33:51 +0800

    update artistService with login function

--- src/main/java/org/iii/seaotter/artist/ArtistService.java
+++ src/main/java/org/iii/seaotter/artist/ArtistService.java
@@ -1,6 +1,5 @@
 package org.iii.seaotter.artist;
 
-
 import java.sql.SQLException;
 import java.util.List;
 
@@ -10,44 +9,46 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ArtistService {
-	
+
 	@Autowired
 	private ArtistDao artistsDao;
-	
+
 	@Autowired
 	private PerformanceDao performanceDao;
-	
+
 	public Artist login(String username) {
-		Artist artist = artistsDao.findByName(username);
-		if (artist != null) {
-			return artist;
+		if (username != null && username.length() != 0) {
+			Artist artist = artistsDao.findByName(username);
+			if (artist != null) {
+				return artist;
+			}
 		}
 		return null;
 	}
-	
-	public Artist getOne(Integer id) throws SQLException {	
+
+	public Artist getOne(Integer id) throws SQLException {
 		return artistsDao.findOne(id);
 	}
-	
-	public List<Artist> getAll() throws SQLException{
+
+	public List<Artist> getAll() throws SQLException {
 		return artistsDao.findAll();
 	}
-	
+
 	public void create(Artist artists) throws SQLException {
 		artistsDao.insert(artists);
 	}
-	
+
 	public void update(Artist artists) throws SQLException {
 		artistsDao.update(artists);
 	}
-	
+
 	public void delete(Integer id) throws SQLException {
-		
+
 		Artist artists = artistsDao.findOne(id);
-		String name = artists.getName();		
-		//performanceDao.delete(name);  // 要額外寫個用name刪除的方法
-		
+		String name = artists.getName();
+		// performanceDao.delete(name); // 要額外寫個用name刪除的方法
+
 		artistsDao.delete(id);
-	}	
+	}
 
 }
