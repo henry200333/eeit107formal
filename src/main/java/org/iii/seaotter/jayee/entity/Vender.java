5ddb979c8b9a984c9da9e27d671defdef3e6ad52
李宜鳴 <@192.168.21.109>
2019-05-16 20:09:16 +0800

    update

--- src/main/java/org/iii/seaotter/jayee/entity/Vender.java
+++ src/main/java/org/iii/seaotter/jayee/entity/Vender.java
@@ -6,7 +6,7 @@ package org.iii.seaotter.jayee.entity;
  *@author ming 李宜鳴  
  * 
  */
-public class Vender {
+public class Vender { 
 	private Long id;
 	private String name;
 	private String address;
