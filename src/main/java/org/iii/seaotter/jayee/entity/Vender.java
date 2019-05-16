54a7ce0f4744f46f4e1101b558be5d4c758fe5d2
李宜鳴 <@192.168.21.109>
2019-05-16 20:08:55 +0800

    update

--- src/main/java/org/iii/seaotter/jayee/entity/Vender.java
+++ src/main/java/org/iii/seaotter/jayee/entity/Vender.java
@@ -12,36 +12,7 @@ public class Vender {
 	private String address;
 	private Integer maxPeople;
 	private String phone;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public Integer getMaxPeople() {
-		return maxPeople;
-	}
-	public void setMaxPeople(Integer maxPeople) {
-		this.maxPeople = maxPeople;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+	
 	
 	
 
