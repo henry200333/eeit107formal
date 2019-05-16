0e9da308afe99f5f100b0c7d167352b078a2a4de
Hsuan-Che Peng <Hsuan-Che Peng@DESKTOP-NFO0ILJ>
2019-05-16 19:15:35 +0800

    fffff

---
+++ src/main/java/org/iii/seaotter/jayee/artist/Artist.java
@@ -0,0 +1,39 @@
+package org.iii.seaotter.jayee.artist;
+
+public class Artist {
+
+	private Integer id;
+	private String name;
+	private java.sql.Date joinDate;
+	private Integer manager;
+	
+	@Override
+	public String toString() {
+		return "Artists [id=" + id + ", name=" + name + ", joinDate=" + joinDate + ", manager=" + manager + "]";
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public java.sql.Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(java.sql.Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	public Integer getManager() {
+		return manager;
+	}
+	public void setManager(Integer manager) {
+		this.manager = manager;
+	}
+	
+}
