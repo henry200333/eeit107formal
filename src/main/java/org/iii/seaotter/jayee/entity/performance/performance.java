6dac410484d13f084ba994f3899d55fed0584582
lcy11 <lcy11@Kallil>
2019-05-16 19:40:52 +0800

    bean by k

---
+++ src/main/java/org/iii/seaotter/jayee/entity/performance/performance.java
@@ -0,0 +1,44 @@
+package org.iii.seaotter.jayee.entity.performance;
+
+/**
+ * 
+ * @author Kallil(李承宇
+ *
+ */
+public class performance {
+
+	private Integer id;
+	private String name;
+	private String url;
+	private Integer activty;
+
+
+	@Override
+		public String toString() {
+			return super.toString();
+		}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Integer getActivty() {
+		return activty;
+	}
+	public void setActivty(Integer activty) {
+		this.activty = activty;
+	}
+}
