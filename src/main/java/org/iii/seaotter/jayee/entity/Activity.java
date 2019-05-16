eebeb42a63bd63223976f91c0b8fbd2c31af7df5
Bennett <Bennett@DESKTOP-1QCDH6B>
2019-05-16 20:34:55 +0800

    @data

--- src/main/java/org/iii/seaotter/jayee/entity/Activity.java
+++ src/main/java/org/iii/seaotter/jayee/entity/Activity.java
@@ -1,13 +1,23 @@
 package org.iii.seaotter.jayee.entity;
+
+import lombok.Data;
+
 /**
  * 
  * @author Bennett 吳帆祥
  *
  */
+
+@Data
 public class Activity {
+	/**
+	 * id XXXXX
+	 */
+	
 	private Long id;
 	private String name;
 	private java.util.Date time;
 	private String article;
+	
 	
 }
