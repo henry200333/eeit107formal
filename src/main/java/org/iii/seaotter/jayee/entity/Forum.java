f2e244eafb8757af9262e7d717ae15498e74a1ba
ken <ken@192.168.100.3>
2019-05-19 21:58:45 +0800

    plus board field

--- src/main/java/org/iii/seaotter/jayee/entity/Forum.java
+++ src/main/java/org/iii/seaotter/jayee/entity/Forum.java
@@ -1,15 +1,23 @@
 package org.iii.seaotter.jayee.entity;
 
+import lombok.Data;
+
 /**
  * 
  * @author Henry劉長軒
  *
  */
 
+@Data
 public class Forum {
 	private Long id;
+	private Board board;
 	private String name;
 	private String context;
 	private java.util.Date commentDate;
+
+	enum Board {
+		Ariticle, Activity, Performance
+	}
 
 }
