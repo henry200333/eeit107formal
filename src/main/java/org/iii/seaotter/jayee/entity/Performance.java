db4a575796d726bc0766b7ad9c2ef67c6218f84f
lcy11 <lcy11@Kallil>
2019-05-23 19:48:15 +0800

    BY K

--- src/main/java/org/iii/seaotter/jayee/entity/Performance.java
+++ src/main/java/org/iii/seaotter/jayee/entity/Performance.java
@@ -1,15 +1,24 @@
 package org.iii.seaotter.jayee.entity;
 
+import lombok.Data;
+
 /**
  * 
  * @author Kallil(李承宇
  *
  */
+@Data
 public class Performance {
 
+
 	private Long id;
+
 	private String name;
+
 	private String url;
+
 	private java.util.Date updateTime;
+
 	private Long activityId;
+
 }
