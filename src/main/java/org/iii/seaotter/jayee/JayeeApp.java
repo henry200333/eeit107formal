7fe7501e98783922117b5e8f63ecd6a90e2ea6cb
Hsuan-Che Peng <Hsuan-Che Peng@DESKTOP-NFO0ILJ>
2019-05-23 19:37:33 +0800

    0523

---
+++ src/main/java/org/iii/seaotter/jayee/JayeeApp.java
@@ -0,0 +1,13 @@
+package org.iii.seaotter.jayee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayeeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JayeeApp.class, args);
+	}
+
+}
---
+++ src/main/java/org/iii/seaotter/jayee/web/AdminArtistController.java
@@ -0,0 +1,42 @@
+package org.iii.seaotter.jayee.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/admin/artist")
+public class AdminArtistController {
+	@RequestMapping("/list")
+	public String listPage() {
+		return "/WEB-INF/jsp/admin/artist-list.jsp";
+
+	}
+
+	@RequestMapping("/add")
+	public String addPage() {
+		return "/WEB-INF/jsp/admin/artist-add.jsp";
+
+	}
+
+	@RequestMapping("/edit")
+	public String editPage() {
+		return "/WEB-INF/jsp/admin/artist-edit.jsp";
+
+	}
+
+	public String insert() {
+		return null;
+
+	}
+
+	public String update() {
+		return null;
+
+	}
+
+	public String delete() {
+		return null;
+
+	}
+}
