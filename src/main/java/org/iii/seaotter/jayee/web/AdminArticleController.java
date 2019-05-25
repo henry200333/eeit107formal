6d2e5d613ea18980aa8a6fd65fae7a65557dc81a
Soma <Soma@Soma-001>
2019-05-25 17:25:27 +0800

    Update AdminArticleController of web.

--- src/main/java/org/iii/seaotter/jayee/web/AdminArticleController.java
+++ src/main/java/org/iii/seaotter/jayee/web/AdminArticleController.java
@@ -1,27 +1,34 @@
 package org.iii.seaotter.jayee.web;
 
+import org.iii.seaotter.jayee.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin/article")
 public class AdminArticleController {
-
+	
+	@Autowired
+	private ArticleService articleService;
+	
 	@RequestMapping("/list")
-	public String listPage() {
-		return "/WEB-INF/jsp/admin/article-list.jsp";
+	public String listPage(Model model) {
+		model.addAttribute("models", articleService.getAll());
+		return "/admin/article-list";
 
 	}
 
 	@RequestMapping("/add")
 	public String addPage() {
-		return "/WEB-INF/jsp/admin/article-add.jsp";
+		return "/admin/article-add";
 
 	}
 
 	@RequestMapping("/edit")
 	public String editPage() {
-		return "/WEB-INF/jsp/admin/article-edit.jsp";
+		return "/admin/article-edit";
 
 	}
 
