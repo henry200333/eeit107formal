fdf0857c47712515e7e70c30658c3b6d9c65f756
Soma <Soma@Soma-001>
2019-05-25 17:02:34 +0800

    Update ArticleService of jayee.service.

---
+++ src/main/java/org/iii/seaotter/jayee/service/ArticleService.java
@@ -0,0 +1,18 @@
+package org.iii.seaotter.jayee.service;
+
+import java.util.List;
+
+import org.iii.seaotter.jayee.dao.ArticleDao;
+import org.iii.seaotter.jayee.entity.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+	@Autowired
+	private ArticleDao articleDao;
+	
+	public List<Article> getAll(){
+		return articleDao.findAll();
+	}
+}
