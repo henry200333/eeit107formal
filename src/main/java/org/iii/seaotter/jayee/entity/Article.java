0f23454cf01d169ece527890cd03dcb15bde1080
Soma <Soma@Soma-001>
2019-05-25 16:19:44 +0800

    Update Article of entities.

--- src/main/java/org/iii/seaotter/jayee/entity/Article.java
+++ src/main/java/org/iii/seaotter/jayee/entity/Article.java
@@ -1,18 +1,29 @@
 package org.iii.seaotter.jayee.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 /**
- * 
  * @author Soma 陳品堯
- *
  */
+@Entity
+@Table(name="Article")
 @Data
 public class Article {
+	@Id
+	@Column(name="id")
 	private Long id;
+	@Column(name="ref_id")
 	private Long refId;
+	@Column(name="name")
 	private String name;
+	@Column(name="type")
 	private Integer type;
+	@Column(name="context")
 	private String context;
 	
 	
