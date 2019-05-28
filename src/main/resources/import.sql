824e4a1dd78ca85472d923847a40f44354d47556
陳品堯 <soma@EEIT>
2019-05-28 20:35:18 +0800

    commit article & import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,16 @@
-  INSERT INTO article (id, name, context, type, ref_id) values
-  (1, '發大財', '我們的最終目標就是要"發大財"', 1, 100),
-  (2, '經濟0分', '政治100分，經濟0分', 1, 105),
-  (3, 'AI贏過放射專家', '肺癌預測的人機對決 谷歌AI贏過6位放射專家', 2, 200);
+  INSERT INTO article (name, context, type, ref_id) values
+  ('發大財', '我們的最終目標就是要"發大財"', 1, 100),
+  ('經濟0分', '政治100分，經濟0分', 1, 105),
+  ('AI贏過放射專家', '肺癌預測的人機對決 谷歌AI贏過6位放射專家', 2, 200);
   
   
  insert into forum (id, board, name, context, comment_date) values
  (1, 1, '薔薔', '大家好', '2019/05/25 18:13:21'),
  (2, 1, '博恩', '我真有那麼尷尬嗎？', '2019/05/22 13:50:02');
  
+
  INSERT INTO performance (id,name,youtube_url,update_time,activity_id) values
- (1,'中正紀念堂舞蹈表演','www.youtube.com','2019/05/25 17:12:05')
- (2,'自家餵狗秀','www.youtube.com','2019/05/28 19:10:35')
- (3,'倒立30分鐘','www.youtube.com','2019/05/28 21:05:46')
+ (1,'中正紀念堂舞蹈表演','www.youtube.com','2019/05/25 17:12:05',1),
+ (2,'自家餵狗秀','www.youtube.com','2019/05/28 19:10:35',1),
+ (3,'倒立30分鐘','www.youtube.com','2019/05/28 21:05:46',1);
  
