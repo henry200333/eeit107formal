fb67193d9addc1669507788591f638e7b9511320
李宜鳴 <@DESKTOP-H0QHHDD>
2019-05-29 20:47:14 +0800

    vender insert

--- src/main/java/org/iii/seaotter/jayee/entity/Vender.java
+++ src/main/java/org/iii/seaotter/jayee/entity/Vender.java
@@ -1,5 +1,10 @@
 package org.iii.seaotter.jayee.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 /**
@@ -7,15 +12,21 @@ import lombok.Data;
  *@author ming 李宜鳴  
  * 
  */
+@Entity
+@Table(name="Vender")
 @Data
 public class Vender {
-	/**
-	 * id primkey
-	 */
-	private Long id; 
+
+	@Id
+	@Column(name="id")
+	private Long id;
+	@Column(name="name")
 	private String name;
+	@Column(name="address")
 	private String address;
+	@Column(name="maxPeople")
 	private Integer maxPeople;
+	@Column(name="phone")
 	private String phone; 
 	
 	
