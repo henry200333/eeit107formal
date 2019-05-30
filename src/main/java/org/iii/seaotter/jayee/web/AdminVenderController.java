0baa8d95d9d9a6a73cf5a8f0a7203b691b15f694
李宜鳴 <@192.168.21.109>
2019-05-30 15:14:21 +0800

    vender

--- src/main/java/org/iii/seaotter/jayee/web/AdminVenderController.java
+++ src/main/java/org/iii/seaotter/jayee/web/AdminVenderController.java
@@ -1,14 +1,24 @@
 package org.iii.seaotter.jayee.web;
 
+import org.iii.seaotter.jayee.service.PerformanceService;
+import org.iii.seaotter.jayee.service.VenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/admin/vender")
 public class AdminVenderController {
+	
+	@Autowired
+	private VenderService venderService;
+	
+	
 	@RequestMapping("/list")
-	public String listPage() {
+	public String listPage(Model model) {
+		model.addAttribute("vender",venderService.getAll() );
 		return "/admin/vender-list";
 
 	}
