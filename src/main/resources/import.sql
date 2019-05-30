d6ebd32fbb90d34427e510cd130c030f130082ce
李宜鳴 <@192.168.21.109>
2019-05-30 13:00:23 +0800

    vender fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,6 @@
    INSERT INTO article (name, context, type, ref_id) values
   ('發大財', '我們的最終目標就是要"發大財"', 1, 100),
   ('經濟0分', '政治100分，經濟0分', 1, 105),
-
   ('AI贏過放射專家', '肺癌預測的人機對決 谷歌AI贏過6位放射專家', 2, 200);
   
  insert into forum (id, board, name, context, comment_date) values
@@ -25,7 +24,7 @@
  (2,'睡覺','柯文哲','2019/05/29 23:35:16','2019/05/30 07:35:47'),
  (3,'打東東','韓國瑜','2019/05/30 07:35:48','2019/06/03 17:15:46');
 
-insert into  Vender (id,name,address,maxpeople,phone) values
+insert into  Vender (id,name,address,max_people,phone) values
 (1,'EZ5音樂餐廳','106台北市大安區安和路二段211號',50,'0227383995'),
 (2,'Music Corner角落音樂餐廳',' 104台北市中山區建國北路一段156號',40,'0225043688'),
 (3,'LOTUS 音樂餐廳','78號長安東路二段中山區台北市10491',60,' 02 2346 8888');
