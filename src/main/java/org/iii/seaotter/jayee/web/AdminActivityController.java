4662befadbca6e17a3cdc0996acaa24e05dbc877
吳帆祥 <vaildiablo448@gmail.com>
2019-06-12 16:38:22 +0800

    先走了掰

--- src/main/java/org/iii/seaotter/jayee/web/AdminActivityController.java
+++ src/main/java/org/iii/seaotter/jayee/web/AdminActivityController.java
@@ -1,19 +1,14 @@
 package org.iii.seaotter.jayee.web;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
+import java.util.Map;
 
 import org.iii.seaotter.jayee.entity.Activity;
-import org.iii.seaotter.jayee.entity.Article;
 import org.iii.seaotter.jayee.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,24 +50,47 @@ public class AdminActivityController {
 	}
 	
 	@PostMapping("/insert")
-	public String insert(@RequestBody List<Activity> activity, Model model) {
-		System.out.println("123");
+	@ResponseBody
+	public Activity insert(@RequestBody Activity activity, Model model) {
 		System.out.println(activity);
-		
-		
+//		Map<String, String> errorMsg = new HashMap<>();
+//		
+//		if(activity.getName()==null || activity.getName().trim()=="") {
+//			System.out.println("name為空");
+//			errorMsg.put("name","請輸入活動名稱");
+//		}
+//		if(activity.getArtist()==null || activity.getArtist().trim()=="") {
+//			errorMsg.put("artist","請輸入表演者");
+//		}
+//		if(activity.getDescription()==null || activity.getDescription().trim()=="") {
+//			errorMsg.put("description","請輸入活動描述");
+//		}
+//		if(activity.getBeginTime()==null) {
+//			errorMsg.put("bt","日期格式錯誤");
+//		}
+//		if(activity.getEndTime()==null) {
+//			errorMsg.put("et","日期格式錯誤");
+//		}
+//		
+//		if(!errorMsg.isEmpty()) {
+//			model.addAttribute("errorMsg", errorMsg);
+//			System.out.println("錯誤訊息不為空");
+//			return activity;
+//		}
 //		activityService.insert(activity);
-		return "redirect:/admin/activity/list";
+//		errorMsg.put("noError","無錯誤");
+//		System.out.println("新增成功");
+		return activityService.insert(activity);
 
 	}
 	@PostMapping("/update")
-	public String update(@Valid@ModelAttribute("activity") Activity activity, BindingResult bindingResult, Model model) {
+	@ResponseBody
+	public Activity update(@RequestBody Activity activity, Model model) {
 		System.out.println(activity);
-		if (bindingResult.hasErrors()) {
-			model.addAttribute("activityParam", activity);
-			return "/admin/activity-edit";
-		}
-		activityService.update(activity);
-		return "redirect:/admin/activity/list";
+		Map<String, String> errorMsg = new HashMap<>();
+				
+		return activityService.update(activity);
+		
 
 	}
 	@PostMapping("/delete")
