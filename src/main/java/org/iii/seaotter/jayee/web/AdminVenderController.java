8b3f7a1f9f6d3bd2484f5961f9690ad8527e9816
李宜鳴 <aaa@aaa>
2019-07-02 16:07:15 +0800

    map  add

--- src/main/java/org/iii/seaotter/jayee/web/AdminVenderController.java
+++ src/main/java/org/iii/seaotter/jayee/web/AdminVenderController.java
@@ -7,17 +7,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
-import org.iii.seaotter.jayee.entity.Forum;
-import org.iii.seaotter.jayee.entity.Performance;
 import org.iii.seaotter.jayee.entity.Vender;
 import org.iii.seaotter.jayee.service.VenderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,7 +41,7 @@ public class AdminVenderController {
 	@RequestMapping("/list")
 
 	public String listPage(Model model) {	
-		model.addAttribute("venders", venderService.getAll());
+//		model.addAttribute("venders", venderService.getAll());
 		return "/admin/vender-list";
 	}
 
@@ -61,10 +65,60 @@ public class AdminVenderController {
 	
 	@RequestMapping("/query")
 	@ResponseBody
-	public List<Vender> query(){	
-	
-		return venderService.getAll();
+	public List<Vender> query(@RequestParam(value="name", defaultValue="") String name,
+			@RequestParam(value="page") Integer page, @RequestParam(value="rows") Integer size){	
+		Pageable pageable=PageRequest.of(page-1, size);
+		Specification<Vender> specification =new Specification<Vender>() {
+			private static final long serialVersionUID = 1L;	
+			@Override
+			public Predicate toPredicate(Root<Vender> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				Predicate predicate=cb.conjunction();
+				if(!StringUtils.isEmpty(name)) {
+					cb.and(cb.like(root.get("name"), "%"+name+"%"));
+				}				
+				return predicate;
+			}
+		};
+		
+		
+		
+		return venderService.getAll(specification,pageable).getContent();
 	}
+	
+	@RequestMapping("/map")
+	@ResponseBody
+	public List<Vender> map(@RequestParam(value="page") Integer page, @RequestParam(value="rows") Integer size,
+			@RequestParam(value="maxlat") Double maxlat,@RequestParam(value="minlat") Double minlat,
+			@RequestParam(value="maxlng") Double maxlng,@RequestParam(value="minlng") Double minlng){
+		
+		Pageable pageable=PageRequest.of(page-1, size);
+		Specification<Vender> specification =new Specification<Vender>() {
+			
+			private static final long serialVersionUID = 1L;	
+			@Override
+			public Predicate toPredicate(Root<Vender> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				Predicate predicate=cb.conjunction();
+				if (!StringUtils.isEmpty(maxlat)&&!StringUtils.isEmpty(minlat)) {
+					System.out.println(maxlat);
+					System.out.println(minlat);
+					predicate = cb.and(cb.between(root.get("lat"),minlat  ,maxlat ));
+				}
+				if (!StringUtils.isEmpty(maxlng)&&!StringUtils.isEmpty(minlng)) {
+					System.out.println(maxlng);
+					System.out.println(minlng);
+					
+					predicate = cb.and(cb.between(root.get("lng"), minlng,  maxlng));
+				}
+					
+				return predicate;
+			}
+		};
+		
+		
+		System.out.println(venderService.getAll(specification,pageable).getContent().size());
+		return venderService.getAll(specification,pageable).getContent();
+	}
+	
 	
 	@PostMapping("/insert")
 	@ResponseBody
